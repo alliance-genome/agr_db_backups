f3b9c0191c03ec31a3720d8459ca9059b33dc448
Manuel Luypaert <luypaert@ebi.ac.uk>
2022-05-11 10:59:04 +0100

    Implemented proper logging

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import logging
 import subprocess
 from datetime import date
 from smart_open import open
@@ -5,6 +6,10 @@ from smart_open import open
 import boto3
 
 def lambda_handler(event, context):
+
+    logger = logging.getLogger(context.function_name)
+
+    logger.info('Retrieving input args...')
 
     DB_ARG_PARAMS = {       #key-value pairs matching {`input_param_name`: `ssm_param_key`}
         'db_host' :     'host',
@@ -15,16 +20,18 @@ def lambda_handler(event, context):
     };
     args_response = get_args_dict(event, DB_ARG_PARAMS)
     if 'err_msg' in args_response:
-        print('Error while retrieving args: '+args_response['err_msg'])
+        logger.error('Error while retrieving args: '+args_response['err_msg'])
         exit
 
     db_args = args_response['db_args']
-    print('env: '+db_args['env'])
-    print('identifier: '+db_args['identifier'])
+    logger.info('env: '+db_args['env'])
+    logger.info('identifier: '+db_args['identifier'])
+    logger.debug('db_args: {}'.format(db_args))
 
+    logger.info('Creating backup to S3...')
     backup_response = backup_postgres_to_s3(db_args)
     if 'err_msg' in backup_response:
-        print('Error while running backup: '+backup_response['err_msg'])
+        logger.error('Error while running backup: '+backup_response['err_msg'])
         exit #TODO: figure out if this is the correct way to indicate failure in lambda
 
     return {
@@ -32,6 +39,9 @@ def lambda_handler(event, context):
     }
 
 def get_args_dict(event, arg_set):
+
+    logger = logging.getLogger(__name__)
+
     #Default values
     return_args = {
         'env': 'dev',
@@ -50,13 +60,17 @@ def get_args_dict(event, arg_set):
         return_args['region'] = event['region']
 
     # Retrieve database details from ssm if not defined directly
+    logger.info('Setting up ssm client...')
     ssm_client = boto3.client('ssm', region_name=return_args['region'])
+    logger.info('Defining ssm parameter name template...')
     ssm_parameter_name = '/{env}/cron/backup/{identifier}/{{}}'.format(env=return_args['env'], identifier=return_args['identifier'])
 
     for arg_key, ssm_key in arg_set.items():
+        logger.debug('\tDefining {}...'.format(arg_key))
         if arg_key in event:
             return_args[arg_key] = event[arg_key]
         else:
+            logger.debug("\t\tRetrieving {} from SSM...".format(arg_key))
             param_name = ssm_parameter_name.format(ssm_key)
             try:
                 param = ssm_client.get_parameter(Name=param_name, WithDecryption=True)
@@ -70,6 +84,8 @@ def get_args_dict(event, arg_set):
 
 def backup_postgres_to_s3(db_args):
 
+    logger = logging.getLogger(__name__)
+
     backup_command = 'PGPASSWORD={PGPASS} pg_dump -Fc -v -h {DB_HOST} -U {DB_USER} -d {DB_NAME}'.format(
         PGPASS=db_args['db_password'], DB_HOST=db_args['db_host'], DB_USER=db_args['db_user'], DB_NAME=db_args['db_name'])
 
@@ -79,6 +95,7 @@ def backup_postgres_to_s3(db_args):
     with open(s3_target, 'wb', transport_params={'client': boto3.client('s3', region_name=db_args['region'])}) as wout:
         process = subprocess.Popen(backup_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
+        logger.info("Streaming backup to {}...".format(s3_target))
         for c in iter(lambda: process.stdout.read(1), b''):
             wout.write(c)
 
