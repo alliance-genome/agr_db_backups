72b3d060c068d769078e0943f3771c2b9fbf9900
Manuel Luypaert <luypaert@ebi.ac.uk>
2022-10-31 12:27:30 +0000

    Updated task role policy to include all required permissions + use POLP, SCRUM-2177

--- cdk_infra/cdk_infra/cdk_infra_stack.py
+++ cdk_infra/cdk_infra/cdk_infra_stack.py
@@ -6,7 +6,8 @@ from aws_cdk import (
     aws_ecs as ecs,
     aws_ecr as ecr,
     aws_ec2 as ec2,
-    aws_iam as iam
+    aws_iam as iam,
+    aws_s3 as s3
 )
 from constructs import Construct
 
@@ -15,6 +16,8 @@ class CdkInfraStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Find the S3 bucket to read/write backups from/to
+        s3_bucket = s3.Bucket.from_bucket_name(self, "AgrDbBackupsBucket","agr-db-backups")
 
         # Create the ECS cluster
         vpc = ec2.Vpc.from_lookup(self, "AgrVpc", vpc_id = "vpc-55522232")
@@ -35,14 +38,32 @@ class CdkInfraStack(Stack):
 
         # Task role defines the AWS permissions required by the container at runtime
         # to run the application
-        #TODO: reduce task role permissions to match POLP (implement policy document used in current template.yaml)
+        task_role_policy_doc = iam.PolicyDocument(
+            statements= [
+                iam.PolicyStatement(
+                    sid="SSMParametersReadAll",
+                    effect=iam.Effect.ALLOW,
+                    actions=[ 'ssm:List*', 'ssm:Describe*', 'ssm:Get*' ],
+                    resources=[ 'arn:aws:ssm:*:100225593120:parameter/*' ]
+                ),
+                iam.PolicyStatement(
+                    sid="S3BucketWriteAll",
+                    effect=iam.Effect.ALLOW,
+                    actions=[ 's3:Put*' ],
+                    resources=[ s3_bucket.bucket_arn+'/*' ]
+                ),
+                iam.PolicyStatement(
+                    sid="S3BucketReadAll",
+                    effect=iam.Effect.ALLOW,
+                    actions=[ 's3:ListBucket*', 's3:Get*' ],
+                    resources=[ s3_bucket.bucket_arn, s3_bucket.bucket_arn+'/*' ]
+                )
+            ]
+        )
+
         task_role = iam.Role(self, "agr-db-backups-task-role",
             assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
-            managed_policies=[
-                iam.ManagedPolicy.from_aws_managed_policy_name("CloudWatchFullAccess"),
-                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess"),
-                iam_ecr_read_policy
-            ]
+            inline_policies= { 'agrDbBackupsTaskRolePolicy' : task_role_policy_doc }
         )
 
         # Task definition
